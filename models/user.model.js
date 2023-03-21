cf809b0bd4b77c60d9cf1b1b97c97b42f7923141
Irfan Hanif <14.irfanh@gmail.com>
2023-03-21 18:20:09 +0700

    added model user

---
+++ models/user.model.js
@@ -0,0 +1,24 @@
+const users = [
+  {
+    id: 1,
+    name: "Adam",
+  },
+  {
+    id: 2,
+    name: "Habibi",
+  },
+  {
+    id: 3,
+    name: "Mahda",
+  },
+  {
+    id: 4,
+    name: "Pajeet",
+  },
+  {
+    id: 5,
+    name: "Samudra",
+  },
+];
+
+module.exports = users;
